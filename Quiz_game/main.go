2880741ec15c57f302662b73b2eee834ed56d692
Alero Awani <aleroawani18@gmail.com>
2023-10-17 11:43:42 +0100

    minor chnages

--- Quiz_game/main.go
+++ Quiz_game/main.go
@@ -6,12 +6,15 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 //Presents quiz problem, accept user input, check for correctness
 
 func main(){
 	csvFilename := flag.String("csv", "problems.csv","a csv file in the format of 'question, answer' ")
+
+	timeLimit := flag.Int("limit", 30, "this is the time limit fo rthe quiz in seconds")
 	flag.Parse()
 
 	file, err := os.Open(*csvFilename)
@@ -25,18 +28,33 @@ func main(){
 	}
 	problems := parseLines(lines)
 
+
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+
+
+	// <-timer.C //here we are waiting for a message from a channel so our code will block until we get that message 
+
 	//print out the problems, get a response from the user then check if the user is correct 
+	// we add a select statement to the for loop that tells us if we get a message from the timer channel then we know that we need to stop the for loop and stop presenting problems 
 	correct := 0
 	for i, p := range problems {
-		fmt.Printf("Problem #%d: %s = \n", i+1,p.q)
-		var answer string
-		fmt.Scanf("%s\n", &answer) //using the reference to answer here to make sure that it has a pointer value to work with so whenever it sets the value we can access it with our variable 
+		select { //when we get a message from the timer channel, the for loop will stop, we dont use a break in this situation because it only breaks out of the select and not the for loop
+			// the return statement breaks out of both
+		case <-timer.C:
+			fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
+			return
+		default: //this proceeds to print out the problem if we dont get the timer message 
+			fmt.Printf("Problem #%d: %s = ", i+1,p.q)
+			var answer string
+			// here scanf blocks our code because it waits for an input from the user, so even if the timer runs out, it will keep waiting until the person puts the answer to the first question, so we have to change it 
+			fmt.Scanf("%s\n", &answer) //using the reference to answer here to make sure that it has a pointer value to work with so whenever it sets the value we can access it with our variable 
 		
-		if answer == p.a {
-			correct++
+			if answer == p.a {
+				correct++
+			}
 		}
+		
 	}
-	fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
 	
 }
 
